d34fce951fe17c4ddc6c0d827973031a7105f2c6
ThiagoDRangel <info.tec.campos@gmail.com>
2023-08-21 09:16:21 -0300

    feat: adicionando funcionalidades

--- controle-candidatos/src/ProcessoSeletivo.java
+++ controle-candidatos/src/ProcessoSeletivo.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 
@@ -7,6 +8,47 @@ public class ProcessoSeletivo {
         System.out.println("Iniciando o Processo seletivo");
         System.out.println("--------------------------------");
         selecaoCandidatos();
+        imprimirSelecionados();
+    }
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu=false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando) {
+                tentativasRealizadas++;
+            }else {
+                System.out.println("Contato realizado com sucesso!!!");
+            }
+        }while(continuarTentando && tentativasRealizadas < 3);
+
+        if(atendeu) {
+            System.out.println("Contato realizado com " + candidato + "na " + tentativasRealizadas + " tentativa(s)");
+        }else {
+            System.out.println("Não foi possível entrar em contato com o candidato " + candidato + ", número de tentativas " + tentativasRealizadas);
+        }
+    }
+
+    //simulando ligações aos candidatos
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
+    }
+
+    static void imprimirSelecionados() {
+        String [] candidatos = {"Jorge", "Carlos", "Miguel", "Thiago", "Julia"};
+        System.out.println("Imprimindo candidatos aprovatos com o índice");
+        for(int indice=0; indice < candidatos.length;indice++) {
+            System.out.println("O candidato n° " + (indice+1) + " é " + candidatos[indice]);
+        }
+        System.out.println("------------------------------------");
+        System.out.println("Forma abreviada de interação usando for each");
+        for(String candidato: candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+            System.out.println("----------------------------------");
+            entrandoEmContato(candidato);
+        }
     }
     static void selecaoCandidatos() {
         String [] candidatos = { "Jorge", "Carlos", "Miguel", "Thiago", "Julia", "Ana, juvenal, Antônio"};
